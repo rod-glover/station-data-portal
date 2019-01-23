6a2a55b9f8ada4a79bc7378843516f6bb7b789c4
Rod Glover <rglover@uvic.ca>
2019-01-23 14:23:08 -0800

    Use BCBaseMap from pcic-react-leaflet-components

--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { FeatureGroup, Circle, Polygon } from 'react-leaflet';
+import { EditControl } from 'react-leaflet-draw';
+import { BCBaseMap } from 'pcic-react-leaflet-components';
 
-import logger from '../../logger';
-import BCMap from "../BCMap";
+import SimpleGeoJSON from '../SimpleGeoJSON';
 
 import './Portal.css';
+
+import logger from '../../logger';
 
 logger.configure({active: true});
 
@@ -13,7 +17,15 @@ class Portal extends Component {
         return (
             <Row className="Portal">
                 <Col lg={10} className="Map">
-                    <BCMap/>
+                  <BCBaseMap viewport={BCBaseMap.initialViewport}>
+                    <FeatureGroup>
+                      <EditControl
+                        position={'topleft'}
+                      />
+                      {/*<Polygon positions={[[50.449219,-127.514648,],[52.426758,-127.514648,],[52.426758,-125.024414,],[50.449219,-125.024414,]]}/>*/}
+                      <SimpleGeoJSON data={{"type":"Feature","properties":{"source":"PCIC Climate Explorer"},"geometry":{"type":"Polygon","coordinates":[[[-127.514648,50.449219],[-127.514648,52.426758],[-125.024414,52.426758],[-125.024414,50.449219],[-127.514648,50.449219]]]}}}/>
+                    </FeatureGroup>
+                  </BCBaseMap>
                 </Col>
                 <Col lg={2} className="Data">
                     <Row>
