edfe61ffabc25e9e4ab14d82702db8c2df12fe34
Rod Glover <rglover@uvic.ca>
2019-02-14 17:10:33 -0800

    Improve date conversion in getStations

--- src/data-services/station-data-service.js
+++ src/data-services/station-data-service.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import urljoin from 'url-join';
 import { flow, tap, identity, map, mapValues, isString } from 'lodash/fp';
+import { mapDeep } from '../utils/fp';
 
 const SDS_URL = process.env.REACT_APP_SDS_URL;
 
 
+// Regex for ISO 8601 date strings; allows YYYY-MM-DD with optional T spec.
 // Now you've got two problems :)
-const ISO_8601 = /(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})/;
+const ISO_8601 = /\d{4}-\d{2}-\d{2}(Td{2}:\d{2}:\d{2})?/;
 
 function transformIso8601Date(value) {
   // If `value` is a string that matches the ISO 8601 date format,
   // transform it to a JS Date.
   // Otherwise return it unmolested.
   const isDateString = isString(value) && ISO_8601.test(value);
-  console.log('8601', value, isDateString)
   return isDateString ?
     new Date(Date.parse(value)) :
     value
@@ -35,7 +36,8 @@ export function getStations(config) {
     urljoin(SDS_URL, 'stations'),
     {
       transformResponse: axios.defaults.transformResponse.concat(
-        map(mapValues(transformIso8601Date))
+        mapDeep(transformIso8601Date)
+        // map(mapValues(transformIso8601Date))
       ),
       ...config,
     },
