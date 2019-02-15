2f918e8deb2b37e167e8ec8ca4d3cc0ebe5eaeab
Rod Glover <rglover@uvic.ca>
2019-02-14 16:34:13 -0800

    Convert date strings in station data to JS Date

--- src/data-services/station-data-service.js
+++ src/data-services/station-data-service.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
 import urljoin from 'url-join';
-
+import { flow, tap, identity, map, mapValues, isString } from 'lodash/fp';
 
 const SDS_URL = process.env.REACT_APP_SDS_URL;
+
+
+// Now you've got two problems :)
+const ISO_8601 = /(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})/;
+
+function transformIso8601Date(value) {
+  // If `value` is a string that matches the ISO 8601 date format,
+  // transform it to a JS Date.
+  // Otherwise return it unmolested.
+  const isDateString = isString(value) && ISO_8601.test(value);
+  console.log('8601', value, isDateString)
+  return isDateString ?
+    new Date(Date.parse(value)) :
+    value
+}
 
 
 export function getNetworks() {
@@ -18,7 +33,12 @@ export function getVariables() {
 export function getStations(config) {
   return axios.get(
     urljoin(SDS_URL, 'stations'),
-    config,
+    {
+      transformResponse: axios.defaults.transformResponse.concat(
+        map(mapValues(transformIso8601Date))
+      ),
+      ...config,
+    },
   );
 }
 
