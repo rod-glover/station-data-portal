5c540ff98563ca4ef50e9d2fb8986c7307eefe30
Rod Glover <rglover@uvic.ca>
2019-07-03 16:26:41 -0700

    Default to Portal C

--- src/components/main/App/App.js
+++ src/components/main/App/App.js
@@ -48,7 +48,7 @@ export default class App extends Component {
                         <Route path={`/${path}`} component={component}/>
                       ))
                     }
-                    <Redirect to={'/B'}/>
+                    <Redirect to={'/C'}/>
                   </Switch>
               </Grid>
             </div>
