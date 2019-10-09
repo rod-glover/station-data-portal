44c12ec2120cc8450c9c66b45bd22bc4b4c7c83f
Rod Glover <rglover@uvic.ca>
2019-10-09 16:25:16 -0700

    Fix failing smoke tests

--- src/components/info/ObservationCounts/__tests__/smoke.js
+++ src/components/info/ObservationCounts/__tests__/smoke.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
-
-jest.mock('../../../data-services/weather-anomaly-data-service');
+import ObservationCounts from '../ObservationCounts';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    ReactDOM.render(<ObservationCounts/>, div);
 });
 
--- src/components/info/StationMetadata/__tests__/smoke.js
+++ src/components/info/StationMetadata/__tests__/smoke.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import StationMetadata from '../StationMetadata';
 
-jest.mock('../../../data-services/weather-anomaly-data-service');
-
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<StationMetadata/>, div);
+    ReactDOM.render(
+      <StationMetadata allNetworks={[]}/>,
+      div
+    );
 });
 
