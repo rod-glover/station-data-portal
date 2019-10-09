c90b0aead7fc8628d9ce033f5aeb196f9ef6d2b4
Rod Glover <rglover@uvic.ca>
2019-10-09 16:26:09 -0700

    Remove cruft from Template

--- src/components/0Template/__tests__/smoke.js
+++ src/components/0Template/__tests__/smoke.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Template from '../Template';
 
-jest.mock('../../../data-services/weather-anomaly-data-service');
-
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Template/>, div);
