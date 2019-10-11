fad08c862f4fa0ea2067bb1052ebfa57980a610f
Rod Glover <rglover@uvic.ca>
2019-10-11 14:35:52 -0700

    Use react-table to render station metadata table

--- src/components/info/StationMetadata/StationMetadata.js
+++ src/components/info/StationMetadata/StationMetadata.js
@@ -1,109 +1,104 @@
+// This component renders a table showing a selected subset of station metadata.
+// This component wraps React Table v6. All props passed to this component are
+// passed into React Table.
+
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
-import map from 'lodash/fp/map';
-import get from 'lodash/fp/get';
+import ReactTable from 'react-table';
 import find from 'lodash/fp/find';
-import isFunction from 'lodash/fp/isFunction';
-import isString from 'lodash/fp/isString';
-
+import FrequencySelector from '../../selectors/FrequencySelector';
 import logger from '../../../logger';
 
+import 'react-table/react-table.css';
 import './StationMetadata.css';
-import FrequencySelector from '../../selectors/FrequencySelector';
+
 
 logger.configure({ active: true });
 
 const formatDate = d => d ? d.toISOString().substr(0,10) : 'unknown';
 
 
-const columns = [
-  {
-    heading: 'Network',
-    getter: (station, networks) => {
-      const network = find({ uri: station.network_uri })(networks);
-      return network ? network.name : '?';
-    },
-  },
-  {
-    heading: 'Native ID',
-    getter: 'native_id'
-  },
-  {
-    heading: 'Station Name',
-    getter: 'histories[0].station_name'
-  },
-  {
-    heading: 'Location',
-    getter: station => {
-      const hx = station.histories[0];
-      return (
-        <div>
-          {-hx.lon} W <br/>
-          {hx.lat} N <br/>
-          Elev. {hx.elevation} m
-        </div>
-      );
-    }
-  },
-  {
-    heading: 'Record',
-    // getter: station => 'record',
-    getter: station => (
-      <div>
-        {formatDate(station.min_obs_time)} to <br/>
-        {formatDate(station.max_obs_time)}
-      </div>
-    )
-  },
-  {
-    heading: 'Obs Freq',
-    getter: station =>
-      FrequencySelector.valueToLabel(station.histories[0].freq),
-  },
-];
-
-function value(getter, station, networks) {
-  if (isString(getter)) {
-    return get(getter, station);
-  }
-  if (isFunction(getter)) {
-    return getter(station, networks);
-  }
-}
-
-class StationMetadata extends Component {
+export default class StationMetadata extends Component {
   static propTypes = {
     stations: PropTypes.array,
     allNetworks: PropTypes.array.isRequired,
   };
 
   render() {
+    const { stations, allNetworks, ...restProps } = this.props;
+
+    const columns = [
+      {
+        id: 'Network',
+        Header: 'Network',
+        minWidth: 80,
+        maxWidth: 100,
+        accessor: station => {
+          const network = find({ uri: station.network_uri })(allNetworks);
+          return network ? network.name : '?';
+        },
+      },
+      {
+        id: 'Native ID',
+        Header: 'Native ID',
+        minWidth: 80,
+        maxWidth: 100,
+        accessor: 'native_id'
+      },
+      {
+        id: 'Station Name',
+        Header: 'Station Name',
+        minWidth: 120,
+        maxWidth: 200,
+        accessor: station => station.histories[0].station_name,
+      },
+      {
+        id: 'Location',
+        Header: 'Location',
+        minWidth: 120,
+        maxWidth: 200,
+        accessor: station => {
+          const hx = station.histories[0];
+          return (
+            <div>
+              {-hx.lon} W <br/>
+              {hx.lat} N <br/>
+              Elev. {hx.elevation} m
+            </div>
+          );
+        }
+      },
+      {
+        id: 'Record',
+        Header: 'Record',
+        minWidth: 100,
+        maxWidth: 200,
+        // accessor: station => 'record',
+        accessor: station => {
+          return (
+          <div>
+            {formatDate(station.min_obs_time)} to <br/>
+            {formatDate(station.max_obs_time)}
+          </div>
+        )}
+      },
+      {
+        minWidth: 80,
+        maxWidth: 100,
+        id: 'Obs Freq',
+        Header: 'Obs Freq',
+        accessor: station =>
+          FrequencySelector.valueToLabel(station.histories[0].freq),
+      },
+    ];
+
     return (
-      <div className={'metadata'}>
-        <Table condensed size={'sm'}>
-          <thead>
-          <tr>
-            { map(col => <th key={col.heading}>{col.heading}</th>)(columns) }
-          </tr>
-          </thead>
-          <tbody>
-          {
-            map(station =>(
-              <tr>
-                {
-                  map(col => (
-                    <td>{value(col.getter, station, this.props.allNetworks)}</td>
-                  ))(columns)
-                }
-              </tr>
-            ))(this.props.stations)
-          }
-          </tbody>
-        </Table>
-      </div>
+      <ReactTable
+        data={stations}
+        columns={columns}
+        defaultPageSize={10}
+        {...restProps}
+      />
     );
   }
 }
-
-export default StationMetadata;
