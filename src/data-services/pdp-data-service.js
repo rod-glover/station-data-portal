75fecf719272925e81cea7fea0a14759029aea83
Rod Glover <rglover@uvic.ca>
2019-10-18 13:21:05 -0700

    Convert all-options selections to empty PDP qp value

--- src/data-services/pdp-data-service.js
+++ src/data-services/pdp-data-service.js
@@ -1,6 +1,7 @@
 import { makeURI } from '../utils/uri';
 import assignAll from 'lodash/fp/assignAll';
 import capitalize from 'lodash/fp/capitalize';
+import filter from 'lodash/fp/filter';
 import flatten from 'lodash/fp/flatten';
 import flow from 'lodash/fp/flow';
 import get from 'lodash/fp/get';
@@ -19,12 +20,19 @@ export const date2pdpFormat = date =>
     `${date.getFullYear()}/${pad2(date.getMonth()+1)}/${pad2(date.getDate())}` :
     '';
 
+
+const allToNone = allOptions => options =>
+  allOptions && options && (options.length === allOptions.length) ? [] : options;
+
+
 const networkSelectorOption2pdpFormat = get('value.name');
 
-export const networkSelectorOptions2pdpFormat = flow(
+export const networkSelectorOptions2pdpFormat = (options, allOptions) => flow(
+  allToNone(allOptions),
   map(networkSelectorOption2pdpFormat),
   join(','),
-);
+)(options);
+
 
 // This function maps a variable item (as returned by the SDP backend
 // `/variables` endpoint) to the representation used by the PDP data download
@@ -35,33 +43,59 @@ export const networkSelectorOptions2pdpFormat = flow(
 // writing why this replacement is performed. For reference, see PDP backend
 // module `pdp_util.filters` and CRMP database view `collapsed_vars_v`,
 // column `vars`.
-export const variable2PdpVariableIdentifier = ({ standard_name, cell_method }) =>
-  standard_name + (cell_method ? cell_method.replace(/time: /g, '_') : '');
+export const variable2PdpVariableIdentifier = (v) =>{
+  if (!v.standard_name) {
+    console.log('### variable2PdpVariableIdentifier: null standard_name', v)
+  }
+  return v.standard_name +
+    (v.cell_method ? v.cell_method.replace(/time: /g, '_') : '');
+};
+// export const variable2PdpVariableIdentifier = ({ standard_name, cell_method }) =>
+//   standard_name + (cell_method ? cell_method.replace(/time: /g, '_') : '');
 
 const variableSelectorOption2pdpFormat = flow(
+  // option.contexts is an array of all variables corresponding to this option.
   get('contexts'),
+  // Filter out variables that lack required attributes.
+  // As far as we know, these variables are erroneous.
+  filter('standard_name'),
   map(variable2PdpVariableIdentifier),
 );
 
-export const variableSelectorOptions2pdpFormat = flow(
+
+// Map an array of variable options to an array of representations of those
+// variables for the PDP backend.
+const variableOptions2pdpFormats = flow(
   map(variableSelectorOption2pdpFormat),
   flatten,
   uniq,
-  join(','),
 );
+
+const flattenGroupedOptions = flow(
+  map('options'),
+  flatten,
+);
+
+export const variableSelectorOptions2pdpFormat = (options, allOptions) => flow(
+  variableOptions2pdpFormats,
+  allToNone(variableOptions2pdpFormats(flattenGroupedOptions(allOptions))),
+  join(','),
+)(options);
 
 const frequencyOption2pdpFormat = get('value');
 
-export const frequencyOptions2pdpFormat = flow(
+export const frequencyOptions2pdpFormat = (options, allOptions) => flow(
   map(frequencyOption2pdpFormat),
+  allToNone(allOptions),
   join(','),
-);
+)(options);
 
 
 export const dataDownloadTarget =
   ({
-     startDate, endDate, networks, variables, frequencies, polygon,
-     clipToDate, onlyWithClimatology, dataCategory, dataFormat
+    startDate, endDate, networks, variables, frequencies, polygon,
+    clipToDate, onlyWithClimatology, dataCategory, dataFormat,
+    allNetworks = [], allVariables = [], allFrequencies = [],
   }) =>
   makeURI(
     `${process.env.REACT_APP_PDP_DATA_URL}/pcds/agg/`,
@@ -69,9 +103,9 @@ export const dataDownloadTarget =
       {
         'from-date': date2pdpFormat(startDate),
         'to-date': date2pdpFormat(endDate),
-        'network-name': networkSelectorOptions2pdpFormat(networks),
-        'input-vars': variableSelectorOptions2pdpFormat(variables),
-        'input-freq': frequencyOptions2pdpFormat(frequencies),
+        'network-name': networkSelectorOptions2pdpFormat(networks, allNetworks),
+        'input-vars': variableSelectorOptions2pdpFormat(variables, allVariables),
+        'input-freq': frequencyOptions2pdpFormat(frequencies, allFrequencies),
         'input-polygon': geoJSON2WKT(polygon),
         'only-with-climatology': onlyWithClimatology ? 'only-with-climatology' : '',
         [`download-${dataCategory}`]: capitalize(dataCategory),
