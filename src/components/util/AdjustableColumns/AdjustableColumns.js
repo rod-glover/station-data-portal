fba880b9177f18a0d0c992e71024d082cb4028f4
Rod Glover <rglover@uvic.ca>
2019-10-22 10:04:58 -0700

    Remove unnecessary lgHiddens state

--- src/components/util/AdjustableColumns/AdjustableColumns.js
+++ src/components/util/AdjustableColumns/AdjustableColumns.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import { Row, Col, Badge, Button, ButtonGroup } from 'react-bootstrap';
 import clone from 'lodash/fp/clone';
 import concat from 'lodash/concat';  // Note: Not FP!
-import fill from 'lodash/fp/fill';
-import map from 'lodash/fp/map';
 import slice from 'lodash/fp/slice';
 import zipAll from 'lodash/fp/zipAll';
 
@@ -29,24 +27,16 @@ export default class AdjustableColumns extends Component {
     }
     const numColumns = this.props.defaultLgs.length;
     this.state = {
-      lgHiddens: map(lg => lg === 0)(props.defaultLgs),
       lgs: clone(props.defaultLgs),
     }
   }
 
   handleShrinkLeftBy = amount => index => () => this.setState(state => {
-    const lgHiddens = state.lgHiddens;
     const lgs = state.lgs;
     const n = lgs.length;
     const amt = Math.min(amount, lgs[index]);
     const newSelfWidth = lgs[index] - amt;
     return {
-      lgHiddens: concat(
-        slice(0, index-1, lgHiddens),
-        false,                // Show L neighbour
-        newSelfWidth === 0,   // Hide self if will be shrunk to zero width
-        slice(index+1, n, lgHiddens),
-      ),
       lgs: concat(
         slice(0, index-1, lgs),
         lgs[index-1] + amt,   // Expand L neighbout
@@ -57,18 +47,11 @@ export default class AdjustableColumns extends Component {
   });
 
   handleShrinkRightBy = amount => index => () => this.setState(state => {
-    const lgHiddens = state.lgHiddens;
     const lgs = state.lgs;
     const n = lgs.length;
     const amt = Math.min(amount, lgs[index]);
     const newSelfWidth = lgs[index] - amt;
     return {
-      lgHiddens: concat(
-        slice(0, index, lgHiddens),
-        newSelfWidth === 0,   // Hide self if will be shrunk to zero width
-        false,                // Show R neighbour
-        slice(index+2, n, lgHiddens),
-      ),
       lgs: concat(
         slice(0, index, lgs),
         newSelfWidth,
@@ -81,13 +64,12 @@ export default class AdjustableColumns extends Component {
   render() {
     const n = this.props.contents.length;
     const lgsContents = zipAll([
-      this.state.lgHiddens,
       this.state.lgs,
       this.props.contents
     ]);
     console.log('### render', lgsContents)
-    const columns = mapWithKey(([lgHidden, lg, content], i) =>
-      <Col lg={lg} lgHidden={lgHidden} style={{
+    const columns = mapWithKey(([lg, content], i) =>
+      <Col lg={lg} lgHidden={lg === 0} style={{
         // borderLeft: i > 0 && '1px dotted #ddd',
         borderRight: i < n && '1px dotted #ddd'
       }}>
