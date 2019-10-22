b1e13089678ee8c025891393af8ff0244b9c1d21
Rod Glover <rglover@uvic.ca>
2019-10-22 12:49:44 -0700

    Fix ClipToDateControl

--- src/components/controls/ClipToDateControl/ClipToDateControl.js
+++ src/components/controls/ClipToDateControl/ClipToDateControl.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Checkbox, FormGroup } from 'react-bootstrap';
 
 import './ClipToDateControl.css';
-import { Checkbox, ControlLabel } from 'react-bootstrap';
 
 export default class ClipToDateControl extends Component {
   static propTypes = {
@@ -13,9 +13,11 @@ export default class ClipToDateControl extends Component {
   render() {
     const { value, ...rest } = this.props;
     return (
-      <Checkbox checked={value} {...rest}>
-        <ControlLabel>Clip time series to filter date range</ControlLabel>
-      </Checkbox>
+      <FormGroup>
+        <Checkbox checked={value} {...rest}>
+          Clip time series to filter date range
+        </Checkbox>
+      </FormGroup>
     );
   }
 }
