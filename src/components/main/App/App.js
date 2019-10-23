0fbf4980d9abbc3ab54ff22123bb608c75682b36
Rod Glover <rglover@uvic.ca>
2019-10-23 13:09:51 -0700

    Put PUBLIC_URL path into Router basename

--- src/components/main/App/App.js
+++ src/components/main/App/App.js
@@ -13,6 +13,12 @@ import PortalE from '../PortalE';
 
 import './App.css';
 
+const getLocation = url => {
+  const a = window.document.createELement('a');
+  a.href = url;
+  return a;
+};
+
 const navSpec = [
   { label: 'Version A', path: 'A', component: PortalD },
   { label: 'Version B', path: 'B', component: PortalE },
@@ -21,36 +27,37 @@ const navSpec = [
 
 export default class App extends Component {
     render() {
-        return (
-          <Router basename={'/#'}>
-            <div>
-              <Navbar fluid>
-                <Nav>
+      const basePath = getLocation(process.env.PUBLIC_URL).pathname;
+      return (
+        <Router basename={`${basePath}/#`}>
+          <div>
+            <Navbar fluid>
+              <Nav>
+                {
+                  navSpec.map(({label, path}) => (
+                    <LinkContainer to={`/${path}`}>
+                      <NavItem eventKey={path}>
+                        {label}
+                      </NavItem>
+                    </LinkContainer>
+                  ))
+                }
+              </Nav>
+            </Navbar>
+
+            <Grid fluid className="App">
+                <Header/>
+                <Switch>
                   {
-                    navSpec.map(({label, path}) => (
-                      <LinkContainer to={`/${path}`}>
-                        <NavItem eventKey={path}>
-                          {label}
-                        </NavItem>
-                      </LinkContainer>
+                    navSpec.map(({path, component}) => (
+                      <Route path={`/${path}`} component={component}/>
                     ))
                   }
-                </Nav>
-              </Navbar>
-
-              <Grid fluid className="App">
-                  <Header/>
-                  <Switch>
-                    {
-                      navSpec.map(({path, component}) => (
-                        <Route path={`/${path}`} component={component}/>
-                      ))
-                    }
-                    <Redirect to={'/A'}/>
-                  </Switch>
-              </Grid>
-            </div>
-          </Router>
+                  <Redirect to={'/A'}/>
+                </Switch>
+            </Grid>
+          </div>
+        </Router>
         );
     }
 }
