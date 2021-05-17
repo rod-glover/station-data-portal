e2ad209b936af23eff9045916c1df5751c6f1c67
rod-glover <rod.glover.ca@gmail.com>
2021-05-17 11:38:20 -0700

    Fix StationMarkers test

--- src/components/maps/StationMarkers/__tests__/smoke.js
+++ src/components/maps/StationMarkers/__tests__/smoke.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
-
-jest.mock('../../../data-services/weather-anomaly-data-service');
+import StationMarkers from '../StationMarkers';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    ReactDOM.render(<StationMarkers/>, div);
 });
 
